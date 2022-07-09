3adb2a75483f974c521bb0ac28bdf47d72f4152f
Amit Meena <amitmeena0206@gmail.com>
2022-07-09 13:14:27 +0530

    project commit by amit

---
+++ src/collectionconcept/Amit.java
@@ -0,0 +1,10 @@
+package collectionconcept;
+
+public class Amit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("amit code runing.....");
+	}
+
+}
