306c07e1cde9215112749d264a653c8aabfebcb5
Kamlesh Kushwah <kamleshkushwah831@gmail.com>
2022-07-15 18:52:29 +0530

    edit by Kamlesh

---
+++ src/collectionconcept/ArrayDemo.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		ArrayList a=new ArrayList();
+		a.add("ram");
+		a.add("kamlesh");
+		System.out.println(a);
+	}
+
+}
