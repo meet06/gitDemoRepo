31b19df51591e351b252f435e9d8484c6d72647a
JatinMalviya999 <108970463+JatinMalviya999@users.noreply.github.com>
2022-07-16 12:53:33 +0530

    Update DevelopClassDemo.java

--- src/collectionconcept/DevelopClassDemo.java
+++ src/collectionconcept/DevelopClassDemo.java
@@ -5,5 +5,8 @@ import java.security.PublicKey;
 public class DevelopClassDemo {
 public static void main(String[] args) {
 	System.out.println("++++++++ABC>COM+++++++++++++++++++");
+	if(!Oxygen){
+	System.out.println("++++++++You will die+++++++++++++++++++");
+	}
 }
 }
