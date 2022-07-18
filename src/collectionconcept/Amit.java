25771d6ba41cf8887c944934b58b361cfd5e73fb
Manish Patidar <manish1998patidar@gmail.com>
2022-07-18 10:11:26 +0530

    sdsadasdas

--- src/collectionconcept/Amit.java
+++ src/collectionconcept/Amit.java
@@ -5,6 +5,7 @@ public class Amit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("amit code runing.....");
+		System.out.println("amit code runing manish.....");
 	}
 
 }
