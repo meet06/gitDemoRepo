113ac05d7f9ab2ef96345cf22842700a1b5a6610
Manish Patidar <manish1998patidar@gmail.com>
2022-07-20 21:51:39 +0530

    edited

--- src/collectionconcept/DevelopClassDemo.java
+++ src/collectionconcept/DevelopClassDemo.java
@@ -3,6 +3,8 @@ package collectionconcept;
 import java.security.PublicKey;
 
 public class DevelopClassDemo {
+private static final boolean Oxygen = false;
+
 public static void main(String[] args) {
 	System.out.println("++++++++ABC>COM+++++++++++++++++++");
 	if(!Oxygen){
